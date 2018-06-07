cbf4a4e57f8a50de9dd675a68fe670a5ed20ed1d
crisgon <kristiano.gon@gmail.com>
2018-06-07 02:37:09 -0300

    Create Box Component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <div id="app">
-
+    <h1>AnimeRank</h1>
+    <ul>
+      <li v-for="anime in animes">
+        <box
+          :img="anime.image_url"
+          :title="anime.title"
+          :release="anime.airing_start"
+          :rank="anime.rank"
+          :score="anime.score"/>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import box from './Box';
 import axios from 'axios'
 export default {
-
+  components: {
+    box
+  },
+  data() {
+    return {
+      animes: null
+    }
+  },
+  mounted() {
+    axios
+      .get('https://api.jikan.moe/top/anime/1')
+      .then(response => response)
+      .then(r => this.animes = r.data.top);
+  }
 }
 </script>
 
---
+++ src/Box.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="box">
+    <img :src="img"/>
+    <div class="info">
+      <h3 class="info-title">{{ title }}</h3>
+      <span class="info-airing">{{ release }}</span>
+      <button>More info</button>
+    </div>
+    <div class="podium">
+      <span class="podium-rank">{{ rank }}º</span>
+      <span class="podium-score">Score: {{ score }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['img', 'title', 'rank', 'score', 'release']
+}
+</script>
+
+<style scoped>
+  .box {
+    width: 100%;
+    max-width: 450px;
+    height: 160px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-around;
+    box-sizing: border-box;
+    padding: .5em;
+    border-radius: 3px;
+    box-shadow: 5px 5px 10px rgba(120,135,182,.1);
+    background-color: #FCFCFC;
+    color: #4F4F4F;
+  }
+
+  .info {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  .info .info-airing {
+    margin-bottom: .5em;
+  }
+
+  .podium {
+    margin-right: 20px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+  }
+
+  .podium-rank {
+    box-sizing: border-box;
+    padding: .5em 0;
+    font-size: 3em;
+    position: relative;
+  }
+
+  .podium-rank:after {
+    content: '';
+    width: 35px;
+    height: 35px;
+    background-image: url('./assets/chevron.svg');
+    position: absolute;
+    top: 10px;
+    right: -35px;
+  }
+
+  .podium-score {
+    position: relative;
+  }
+
+  .podium-score:after {
+    content: '';
+    width: 20px;
+    height: 20px;
+    background-image: url('./assets/star.svg');
+    position: absolute;
+    top: -2px;
+    right: -22px;
+  }
+</style>
+
